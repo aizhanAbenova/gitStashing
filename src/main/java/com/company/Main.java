f7a08c582532b891c0f831f528e4bcd51ec8f620
Aizhan Abenova <aizhan.zh.ab.07@gmail.com>
2022-05-15 11:03:30 -0400

    the basic structure of the car is ready, working on advance features

---
+++ src/main/java/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import com.company.basicFeatures;
+
+public class Main {
+    public static void main(String[] args) {
+        //write your code here
+        basicFeatures car=new basicFeatures();
+
+    }
+}
